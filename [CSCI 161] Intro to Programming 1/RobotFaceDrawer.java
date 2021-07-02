29beef6cc0484b5e312078df51f228e80f0b0737
Nicholas Bellman <58608676+nrbellman@users.noreply.github.com>
2021-07-02 01:47:23 -0400

    Add files via upload

---
+++ [CSCI 161] Intro to Programming 1/RobotFaceDrawer.java
@@ -0,0 +1,112 @@
+/**
+ * Draws a sideways robot face.
+ * 
+ * @author Nick Bellman
+ *
+ */
+public class RobotFaceDrawer 
+{
+
+	/**
+	 * Assembles the separate parts of the robot's face.
+	 */
+	public static void main(String[] args) 
+	{
+		
+		drawTriangleUp();
+		drawDiamond();
+		
+		System.out.print("\n");
+		drawCheckedPattern();
+		System.out.print("\n");
+		
+		drawDiamond();
+		drawTriangleDown();
+		
+	}
+	
+	/**
+	 * Draws a upward pointing triangle.
+	 */
+	public static void drawTriangleUp() 
+	{
+		
+		System.out.println("     *");
+		System.out.println("    / \\");
+		System.out.println("   /   \\");
+		System.out.println("  /     \\");
+		System.out.println(" /       \\");
+		System.out.println("/         \\");
+		
+	}
+	
+	/**
+	 * Draws a downward pointing triangle.
+	 */
+	public static void drawTriangleDown() 
+	{
+		
+		System.out.println("\\         /");
+		System.out.println(" \\       /");
+		System.out.println("  \\     /");
+		System.out.println("   \\   /");
+		System.out.println("    \\ /");
+		System.out.println("     *");
+		
+	}
+	
+	/**
+	 * Draws a diamond using an upward triangle and a downwards triangle.
+	 */
+	public static void drawDiamond() 
+	{
+		
+		drawTriangleUp();
+		drawTriangleDown();
+		
+	}
+	
+	/**
+	 * Draws a checked line of hashes with the first character being a hash.
+	 */
+	public static void drawCheckedLineStartsFilled() 
+	{
+		
+		System.out.println("# # # # # # # ");
+		
+		// By writing this method, it allows us to make any changes to drawCheckedLineStartsEmpty, 
+		// and by extension drawCheckedPattern in only one place.
+		
+	}
+	
+	/**
+	 * Draws a checked line of hashes with the first character being a space.
+	 */
+	public static void drawCheckedLineStartsEmpty() 
+	{
+		
+		System.out.print(" ");
+		drawCheckedLineStartsFilled();
+		
+	}
+	
+	/**
+	 * Draws a checked pattern using alternating checked lines.
+	 */
+	public static void drawCheckedPattern() 
+	{
+		
+		drawCheckedLineStartsFilled();
+		drawCheckedLineStartsEmpty();
+		drawCheckedLineStartsFilled();
+		drawCheckedLineStartsEmpty();
+		drawCheckedLineStartsFilled();
+		drawCheckedLineStartsEmpty();
+		drawCheckedLineStartsFilled();
+		drawCheckedLineStartsEmpty();
+		drawCheckedLineStartsFilled();
+		
+	}
+	
+}
+
