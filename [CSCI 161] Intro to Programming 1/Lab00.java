f7370cfaf040075f6958f0f2ca1f65f766c4b543
Nicholas Bellman <58608676+nrbellman@users.noreply.github.com>
2021-07-02 01:44:09 -0400

    Add files via upload

---
+++ [CSCI 161] Intro to Programming 1/Lab00.java
@@ -0,0 +1,40 @@
+
+public class Lab00 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("I am writing a program.");
+		System.out.println("Nicholas Bellman\n");
+		System.out.println("Pick up bread bag.");
+		System.out.println("Locate twist tie.");
+		System.out.println("Undo twist tie");
+		System.out.println("Take two slices of bread from open end of bread bag");
+		System.out.println("Place slices of bread on plate");
+		System.out.println("Locate Peanut Butter jar");
+		System.out.println("Pick up Peanut butter jar");
+		System.out.println("Grab Peanut Butter jar lid");
+		System.out.println("Unscrew Peanut Butter jar lid from Peanut Butter jar");
+		System.out.println("Lift lid off of Peanut Butter jar");
+		System.out.println("Put down Peanut Butter jar");
+		System.out.println("Locate Jelly jar");
+		System.out.println("Pick up Jelly jar");
+		System.out.println("Grab Jelly jar lid");
+		System.out.println("Unscrew Jelly jar lid from Jelly jar");
+		System.out.println("Lift lid off of Jelly jar");
+		System.out.println("Put down Jelly jar");
+		System.out.println("Locate knife");
+		System.out.println("Grab knife");
+		System.out.println("Insert blade of knife into top of Peanut Butter jar");
+		System.out.println("Scoop out Peanut Butter from Peanut Butter jar");
+		System.out.println("Apply Peanut Butter on knife to the wide side of bread slice 1");
+		System.out.println("spread Peanut Butter");
+		System.out.println("Insert blade of knife into top of Jelly jar");
+		System.out.println("Scoop out Jelly from Jelly jar");
+		System.out.println("Apply Jelly on knife to the wide side of bread slice 2");
+		System.out.println("Spread Jelly");
+		System.out.println("Put down knife");
+		System.out.println("Grab bread slice 1");
+		System.out.println("Lift bread slice 1");
+		System.out.println("Place side of bread slice 1 with Peanut Butter to side of slice of bread 2 with jelly");
+	}
+}
